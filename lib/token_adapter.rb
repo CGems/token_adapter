5446fd4d7dbdb7f39d60054b207ded39b4afd831
wumz <wumz@kexinbao100.com>
2017-09-27 09:37:39 +0800

    0.2.13 remove provider base

--- lib/token_adapter.rb
+++ lib/token_adapter.rb
@@ -23,11 +23,11 @@ module TokenAdapter
 end
 
 require "token_adapter/json_rpc"
-require "token_adapter/ethereum/provider/base"
 require "token_adapter/ethereum/provider/etherscan"
 require "token_adapter/ethereum/provider/infura"
 require "token_adapter/ethereum/provider/rpc"
 require "token_adapter/version"
+require "token_adapter/base"
 require "token_adapter/ethereum/eth"
 require "token_adapter/ethereum/erc20"
 require "token_adapter/ethereum/atm"
