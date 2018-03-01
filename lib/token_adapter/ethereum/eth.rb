8d116da0f5460309c5a3376f971dcc01188dd138
Wu Minzhe <wuminzhe@126.com>
2018-03-01 11:16:06 +0800

    update

--- lib/token_adapter/ethereum/eth.rb
+++ lib/token_adapter/ethereum/eth.rb
@@ -160,9 +160,7 @@ module TokenAdapter
 
         gas_price_in_dec = gas_price.nil? ? eth_gas_price.to_i(16) : gas_price
 
-        if gas_price_in_dec < 5_000_000_000
-          gas_price_in_dec = 5_000_000_000
-        end
+        gas_price_in_dec = 5_000_000_000 if gas_price_in_dec < 5_000_000_000
 
         nonce = nonce.nil? ? eth_get_transaction_count(address, 'pending') : nonce
         args = {
@@ -285,12 +283,9 @@ module TokenAdapter
           v = nil
         end
 
-
-        gas_price_in_hex = gas_price.nil? ? eth_gas_price : dec_to_hex(gas_price) 
-
-        if gas_price_in_hex.to_i(16) < 5_000_000_000
-          gas_price_in_hex = dec_to_hex(5_000_000_000)
-        end
+        gas_price_in_dec = gas_price.nil? ? eth_gas_price.to_i(16) : gas_price
+        gas_price_in_dec = 5_000_000_000 if gas_price_in_dec < 5_000_000_000
+        gas_price_in_hex = dec_to_hex(gas_price_in_dec)
 
         eth_send_transaction(
             (from.nil? ? nil : from),
