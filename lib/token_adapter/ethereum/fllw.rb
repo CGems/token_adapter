22b20313ecd14337e36ee80a5a6f29b702405f0b
lixiumiao <lixiumiao@blockchainrnd.org>
2018-03-15 11:32:48 +0800

    update fllw config

--- lib/token_adapter/ethereum/fllw.rb
+++ lib/token_adapter/ethereum/fllw.rb
@@ -1,8 +1,10 @@
 module TokenAdapter
   module Ethereum
-    class Fllw < Erc20
+    class Fllw < Erc223
       def initialize(config)
         super(config)
+        @token_contract_address = '0x0200412995f1bafef0d3f97c4e28ac2515ec1ece'
+        @token_decimals = 18
       end
     end
   end
--- lib/token_adapter/version.rb
+++ lib/token_adapter/version.rb
@@ -1,3 +1,3 @@
 module TokenAdapter
-  VERSION = "0.2.81"
+  VERSION = "0.2.82"
 end
