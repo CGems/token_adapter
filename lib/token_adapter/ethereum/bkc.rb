9cb512e24122cd34607fcf66dda385420d842896
Wu Minzhe <wuminzhe@gmail.com>
2018-06-13 12:56:35 +0800

    update bkc contract address

--- lib/token_adapter/ethereum/bkc.rb
+++ lib/token_adapter/ethereum/bkc.rb
@@ -3,7 +3,7 @@ module TokenAdapter
     class Bkc < Erc20
       def initialize(config)
         super(config)
-        @token_contract_address = '0x262377e5956f2fc32d8024c0c9282fe0c53da24c'
+        @token_contract_address = '0x9fAB652aBd7651E7f46D18522eE4710d9214F8A8'
         @token_decimals = 8
       end
     end
