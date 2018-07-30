a4e9380a96ea6d3b3f72cd3a6774affd42d96c57
tarzansos <tuminfei1981@gmail.com>
2018-07-30 15:07:07 +0800

    修改mox地址

--- lib/token_adapter/bilur/mox.rb
+++ lib/token_adapter/bilur/mox.rb
@@ -3,7 +3,7 @@ module TokenAdapter
     class Mox < Erc40
       def initialize(config)
         super(config)
-        @token_contract_address = '0x1f69920a1687197e62b8db1226eb7ce5b0614387'
+        @token_contract_address = '0x29f9da254ed1b33ede8edf6d87e988bfd1b9ad06'
         @token_decimals = 8
       end
     end
