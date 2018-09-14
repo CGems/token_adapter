638f1e546cfe1e57ae9169a52894507b7a093930
wuminzhe <wuminzhe@gmail.com>
2018-09-14 10:31:47 +0800

    add property_id to config of usdt

--- lib/token_adapter/usdt.rb
+++ lib/token_adapter/usdt.rb
@@ -9,11 +9,12 @@ module TokenAdapter
     def initialize(config)
       super(config)
       @rpc = config[:rpc]
+      @property_id = config[:property_id] || USDT_PROPERTY_ID
     end
 
     def getbalance(address = nil)
       address ||= from
-      balance_info = fetch(method: 'omni_getbalance', params: [address, USDT_PROPERTY_ID])
+      balance_info = fetch(method: 'omni_getbalance', params: [address, @property_id])
       return balance_info['balance']
     end
 
@@ -22,20 +23,20 @@ module TokenAdapter
     end
 
     def sendtoaddress(address, amount)
-      txhash = fetch(method: 'omni_send', params: [from, address, USDT_PROPERTY_ID, amount.to_s])
+      txhash = fetch(method: 'omni_send', params: [from, address, @property_id, amount.to_s])
       raise TxHashError, 'txhash is nil' unless txhash
       txhash
     end
     
     def wallet_collect(address, amount)
-      txhash = fetch(method: 'omni_send', params: [address, from, USDT_PROPERTY_ID, amount.to_s])
+      txhash = fetch(method: 'omni_send', params: [address, from, @property_id, amount.to_s])
       raise TxHashError, 'txhash is nil' unless txhash
       txhash
     end
 
     def gettransaction(txid)
       tx = fetch(method: 'omni_gettransaction', params: [txid])
-      raise UnsupportedProperty, 'Unsupported property' if tx['propertyid'] != USDT_PROPERTY_ID
+      raise UnsupportedProperty, 'Unsupported property' if tx['propertyid'] != @property_id
       tx['timereceived'] = tx['blocktime'] || Time.now.to_i
       # 保持和btc返回结构一致
       tx['details'] = [
