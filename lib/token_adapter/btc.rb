73edfbabf8f59d118bf37b1a29c7cf869626e75f
Wu Minzhe <wuminzhe@gmail.com>
2018-09-18 17:41:53 +0800

    btc getbalance支持单地址查询

--- lib/token_adapter/btc.rb
+++ lib/token_adapter/btc.rb
@@ -7,6 +7,36 @@ module TokenAdapter
       @rpc = config[:rpc]
     end
 
+
+    # [  
+    #   {  
+    #     "txid":"ad25ddd671db5d3170b2bb47f391fd4a524b3da2b0b3fcdc044fb698d7b0a002",
+    #     "vout":0,
+    #     "address":"mo6AmvaNz8GhQPJPYaf75d9h4SppFfve15",
+    #     "account":"",
+    #     "scriptPubKey":"76a914531106837f2d7f691f0a248378b2cccf8d17a80388ac",
+    #     "amount":0.00000555,
+    #     "confirmations":61413,
+    #     "spendable":true,
+    #     "solvable":true
+    #   },
+    #   ...
+    # ]
+    def getbalance(address=nil)
+      if address
+        utxos = fetch(method: 'listunspent', params: [])
+        utxos.reduce(0) do |sum, utxo|
+          if utxo['address'] == address
+            sum + utxo['amount']
+          else
+            sum
+          end
+        end
+      else
+        return fetch(method: 'getbalance', params: [])
+      end
+    end
+
     def getnewaddress(account, passphrase)
       return fetch(method: 'getnewaddress', params: ['payment'])
     end
